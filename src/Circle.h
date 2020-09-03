c095666dca7cf5b12501f14505b1d4d3073c2b22
Ignacio Rivera <ignacio.rivera1@upr.edu>
2020-09-03 18:58:26 -0400

    Added Class Circles and Added .gitignore

---
+++ src/Circle.h
@@ -0,0 +1,29 @@
+class Circle{
+    private:
+        double r;
+        double d;
+    public:
+
+    Circle(double radius, double diameter){
+        this->r = radius;
+        this->d = diameter;
+    }
+    
+    void setRadius(double radius){
+        this->r = radius;
+    }
+
+    void setDiameter(double diameter){
+        this->d = diameter;
+    }
+
+    double getRadius(){
+        return this->r;
+    }
+
+    double getDiameter(){
+        return this->d;
+    }
+
+
+};
